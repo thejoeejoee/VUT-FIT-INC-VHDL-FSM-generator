b8a6b235501150dfc19cd6deca5b2bc0686bec6a
Josef Kolář <thejoeejoee@gmail.com>
2017-05-03 14:01:02 +0200

    generator logic [WIP]

--- fsm_generator.py
+++ fsm_generator.py
@@ -1,29 +1,46 @@
 #!/usr/bin/env python3
 # coding=utf-8
-from sys import stderr
+from functools import partial
+from itertools import zip_longest
+from operator import is_not
 from typing import Optional, Iterable
 
 FILE_PATTERN = """
 architecture behavioral of fsm is
     type t_state is (
-        {states_enum}, FINISH
+        {states_enum},
+        FINISH
     );
     signal present_state, next_state : t_state;
 begin
 -- -------------------------------------------------------
 next_state_logic : process(present_state, KEY, CNT_OF)
 begin
-   case (present_state) is
+   case ({current_state_name}) is
    {code_states}
 
-   when {wrong_state} =>
-      -- TODO
-   when {print_success} =>
-      -- TODO
-   when {print_fail} =>
-      -- TODO
-   when FINISH =>
-      -- TODO
+    when {wrong_state} =>
+        {next_state_name} <= {wrong_state};
+        if KEY(15) = '1' then
+            {next_state_name} <= {print_fail};
+        end if;
+    when {print_success} =>
+        {next_state_name} <= {print_fail};
+        if (CNT_OF = '1') then
+            {next_state_name} <= {finish_state_name};
+        end if;
+    when {print_fail} =>
+        {next_state_name} <= {print_fail};
+        if (CNT_OF = '1') then
+            {next_state_name} <= {finish_state_name};
+        end if;
+    when {finish_state_name} =>
+        {next_state_name} <= {finish_state_name};
+        if KEY(15) = '1' then
+            {next_state_name} <= {first_code_state};
+        end if;
+    when others =>
+        {next_state_name} <= {first_code_state};
    end case;
 end process next_state_logic;
 end architecture behavioral;
@@ -35,22 +52,28 @@ class Config(object):
     Holds loaded generator configuration.
     """
 
-    def __init__(self):
-        self.code1 = self.code2 = ""
-        self.next_state_name = self.current_state_name = self.code_state_pattern = ""
-        self.wrong_code_state = self.print_success_state = self.print_fail_state = ""
+    code1 = code2 = ""
+    next_state_name = 'next_state'
+    current_state_name = 'current_state'
+    code_state_pattern = 'test_{}'
+    finish_state_name = 'finish'
+    wrong_code_state = 'wrong_code_state'
+    print_success_state = "print_success"
+    print_fail_state = 'print_fail'
+    output_file = 'fsm_state.vhd'
 
     @classmethod
     def load(cls):
         conf = Config()
-        conf.next_state_name = input("Next state signal name [next_state]: ").strip() or 'next_state'
-        conf.current_state_name = input("Current state signal name [current_state]: ").strip() or 'current_state'
-        conf.code_state_pattern = input("Code state pattern [test_{}]: ").strip() or 'test_{}'
+        conf.next_state_name = input("Next state signal name [next_state]: ").strip() or cls.next_state_name
+        conf.current_state_name = input("Current state signal name [current_state]: ").strip() or cls.current_state_name
+        conf.code_state_pattern = input("Code state pattern [test_{}]: ").strip() or cls.code_state_pattern
 
-        conf.wrong_code_state = input("Wrong code state name [wrong_code]: ").strip() or 'wrong_code_state'
-        conf.print_success_state = input("State to print success [print_success]: ").strip() or 'print_success'
-        conf.print_fail_state = input("State to print success [print_fail]: ").strip() or 'print_fail'
-        conf.output_file = input("File to write [fsm_state.vhd]: ").strip() or 'fsm_state.vhd'
+        conf.wrong_code_state = input("Wrong code state name [wrong_code]: ").strip() or cls.wrong_code_state
+        conf.print_success_state = input("State to print success [print_success]: ").strip() or cls.print_success_state
+        conf.print_fail_state = input("State to print success [print_fail]: ").strip() or cls.print_fail_state
+        conf.finish_state_name = input("State to finish [finish]: ").strip() or cls.finish_state_name
+        conf.output_file = input("File to write [fsm_state.vhd]: ").strip() or cls.output_file
 
         conf.code1 = input("Code 1: ").strip()
         conf.code2 = input("Code 2: ").strip()
@@ -63,21 +86,19 @@ class Config(object):
 
 class State(object):
     _NEXT_STATE_BRANCH = """
-    if (KEY({key}) = '1') then
-        {next_state_name} <= {next_state};
-    endif;
-    """
+        if (KEY({key}) = '1') then
+            {next_state_name} <= {next_state};
+        end if;"""
 
     PATTERN = """
     when {current_state} =>
         {next_state_name} <= {current_state};
-    {next_state_branch}
         if KEY(15) = '1' then
             {next_state_name} <= {print_fail};
         elsif KEY(15 downto 0) /= "0000000000000000" then
             {next_state_name} <= {wrong_code_state};
-        end if;
-    """
+        {next_state_branch}
+        end if;"""
     PATTERN_LAST_STATE = """
     when {current_state} =>
         {next_state_name} <= {current_state};
@@ -85,31 +106,31 @@ class State(object):
             {next_state_name} <= {print_success};
         elsif KEY(15 downto 0) /= "0000000000000000" then
             {next_state_name} <= {wrong_code_state};
-        end if;
-    """
+        end if;"""
 
-    def __init__(self, current_state_number: str, next_state_number: Optional[str],
-                 config: Config, code_index: int, splitter: Optional[Iterable[str]] = None):
-        self.current = current_state_number
-        self.next = next_state_number
+    def __init__(self, next_state_key: Optional[str], char_index: int,
+                 config: Config, code_index: int, splitter: Optional[Iterable[str]] = None, is_last: bool = False):
+        self.next_state_key = next_state_key
+        self.char_index = char_index
         self.config = config
         self.code_index = code_index
         self.splitter = splitter
+        self.is_last = is_last
 
     @classmethod
-    def state_name(cls, config: Config, index: int, symbol: str):
-        return config.code_state_pattern.format('_'.join(map(str, filter(None, (
-            index,
-            symbol
+    def state_name(cls, config: Config, code_index: int, symbol_index: int):
+        return config.code_state_pattern.format('_'.join(map(str, filter(partial(is_not, None), (
+            symbol_index,
+            code_index
         )))))
 
     @property
     def current_state(self):
-        return self.state_name(self.config, self.code_index, self.current)
+        return self.state_name(self.config, self.code_index, self.char_index)
 
     @property
     def next_state(self):
-        return self.state_name(self.config, self.code_index, self.next)
+        return self.state_name(self.config, self.code_index, self.char_index + 1)
 
     @property
     def vhdl(self):
@@ -124,10 +145,10 @@ class State(object):
                 next_state_branch='\n'.join(self._NEXT_STATE_BRANCH.format(
                     key=next_state_key,
                     next_state_name=self.config.next_state_name,
-                    next_state=self.state_name(config, index, next_state_key)
+                    next_state=self.state_name(config, index, self.char_index + 1)
                 ) for index, next_state_key in enumerate(self.splitter, 1))
             )
-        elif self.next:
+        elif not self.is_last:
             return self.PATTERN.format(
                 current_state=self.current_state,
                 current_state_name=self.config.current_state_name,
@@ -136,7 +157,7 @@ class State(object):
                 print_fail=self.config.print_fail_state,
                 next_state_name=self.config.next_state_name,
                 next_state_branch=self._NEXT_STATE_BRANCH.format(
-                    key=self.next,
+                    key=self.next_state_key,
                     next_state_name=self.config.next_state_name,
                     next_state=self.next_state
                 )
@@ -152,47 +173,74 @@ class State(object):
 
 
 if __name__ == '__main__':
+    """
     config = Config.load()
     while not config:
         print("Invalid config.", file=stderr)
         config = Config.load()
+    """
+    config = Config()
+    config.code1 = '42'
+    config.code2 = '48'
+    config.output_file = '-'
 
     states = []
-    for i, (num1, num2) in enumerate(zip(config.code1, config.code2)):
+    split = False
+    for i, (num1, num2) in tuple(enumerate(zip_longest(config.code1, config.code2))):
         is_next_same = config.code1[i + 1: i + 2] == config.code2[i + 1: i + 2]
-        if num1 == num2:
-            state = State(
-                current_state_number=num1,
-                next_state_number=config.code1[i + 1: i + 2] if is_next_same else None,
-                config=config,
-                code_index=0,
-                splitter=(
-                    config.code1[i + 1: i + 2],
-                    config.code2[i + 1: i + 2],
-                ) if not is_next_same else None
-            )
+        if num1 == num2 and not split:
+            if is_next_same:
+                state = State(
+                    next_state_key=config.code1[i + 1: i + 2],
+                    char_index=i,
+                    config=config,
+                    code_index=0
+                )
+            else:
+                state = State(
+                    next_state_key=None,
+                    char_index=i,
+                    config=config,
+                    code_index=0,
+                    splitter=(
+                        config.code1[i + 1: i + 2],
+                        config.code2[i + 1: i + 2],
+                    )
+                )
             states.append(state)
             continue
 
-        state1 = State(
-            current_state_number=num1,
-            next_state_number=config.code1[i + 1: i + 2],
-            config=config,
-            code_index=1,
-        )
-        state2 = State(
-            current_state_number=num2,
-            next_state_number=config.code2[i + 1: i + 2],
-            config=config,
-            code_index=2,
-        )
-        states.extend((state1, state2))
+        split = True
+        if num1:
+            states.append(State(
+                next_state_key=config.code1[i + 1: i + 2],
+                char_index=i,
+                config=config,
+                code_index=1,
+                is_last=len(config.code1) - 1 == i
+            ))
+        if num2:
+            states.append(State(
+                next_state_key=config.code2[i + 1: i + 2],
+                char_index=i,
+                config=config,
+                code_index=2,
+                is_last=len(config.code2) - 1 == i
+            ))
 
-    print(FILE_PATTERN.format(
-        code_states='\n\n'.join(state.vhdl for state in states),
-        states_enum=', \n'.join(state.current_state for state in states),
+    generated = FILE_PATTERN.format(
+        code_states='\n'.join(state.vhdl for state in states),
+        states_enum=', \n\t\t'.join(state.current_state for state in states),
         wrong_state=config.wrong_code_state,
         print_success=config.print_success_state,
         print_fail=config.print_fail_state,
-
-    ))
+        next_state_name=config.next_state_name,
+        current_state_name=config.current_state_name,
+        finish_state_name=config.finish_state_name,
+        first_code_state=State.state_name(config, 0, 0)
+    )
+    if config.output_file == '-':
+        print(generated)
+    else:
+        with open(config.output_file, 'w') as f:
+            f.write(generated)
